f3e03f435e26c01e53a70321a204953839a3f715
minhlexuan1909 <minhlx1909@gmail.com>
2022-11-01 16:16:56 +0700

    [UPDATE] Show student data (Right under row version)

--- src/features/dataAnalyzing/components/admin/AdminTable.jsx
+++ src/features/dataAnalyzing/components/admin/AdminTable.jsx
@@ -1,9 +1,10 @@
-import { Button, Table, Tag } from "antd";
+import { Button, Card, Table, Tag } from "antd";
 import ColumnGroup from "antd/lib/table/ColumnGroup";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { dataExperimentSelector } from "../../services/dataAnalyzingSlice";
 import { dataAnalyzingActions } from "../../services/dataAnalyzingSlice.js";
+import StudentData from "./StudentData";
 
 const AdminTable = (props) => {
   const { isShowStudentData, setIsShowStudentData } = props;
@@ -45,19 +46,20 @@ const AdminTable = (props) => {
         </>
       ),
     },
-    {
-      title: "Action",
-      key: "action",
-      render: (_, record) => (
-        <Button
-          id={record.id}
-          onClick={() => handleShowBtnOnClick(record.id)}
-          type="primary"
-        >
-          Xem kết quả
-        </Button>
-      ),
-    },
+    Table.EXPAND_COLUMN,
+    // {
+    //   title: "Action",
+    //   key: "action",
+    //   render: (_, record) => (
+    //     <Button
+    //       id={record.id}
+    //       onClick={() => handleShowBtnOnClick(record.id)}
+    //       type="primary"
+    //     >
+    //       Xem kết quả
+    //     </Button>
+    //   ),
+    // },
   ];
 
   const handleOnChangeSelection = (e) => {
@@ -75,6 +77,26 @@ const AdminTable = (props) => {
       dataSource={dataTable}
       columns={columns}
       rowKey={(record) => record.id}
+      expandable={{
+        expandIcon: ({ expanded, onExpand, record }) =>
+          expanded ? (
+            <Button type="primary" onClick={(e) => onExpand(record, e)}>
+              Xem kết quả
+            </Button>
+          ) : (
+            <Button type="primary" onClick={(e) => onExpand(record, e)}>
+              Xem kết quả
+            </Button>
+          ),
+        expandedRowRender: (record) => (
+          <Card title={`Thông tin của ${record.id}`} key={record.id}>
+            <StudentData id={record.id} dataExperiment={dataExperiment} />
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Button type="primary">Xuất file</Button>
+            </div>
+          </Card>
+        ),
+      }}
     ></Table>
   );
 };
